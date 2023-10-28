5ec19e0b4aa3235842a05c7a3014d510c3777beb
eca-kubi <ecakubi@gmail.com>
2023-10-28 08:37:59 +0000

    Update Azure Secret Identifier URI

--- app/config/secrets-example.php
+++ app/config/secrets-example.php
@@ -3,12 +3,14 @@
  * Don't forget to provide the right values that apply to your deployment environment.
  * See below examples of values you must provide.
  */
-const DB_PASS = 'YOUR DATABASE PASSWORD';
+const DB_PASSWORD = 'YOUR DATABASE PASSWORD';
 
 # You must create an email client app in your email account and use the app password here
 # The password you created for the email client app in your email account
 const EMAIL_CLIENT_APP_PASSWORD = 'YOUR EMAIL CLIENT APP PASSWORD';
 
+const PRIVATE_KEY_VALUE = 'YOUR PRIVATE KEY VALUE';
+
 # A private key used to encrypt and decrypt data
-# Use a secure hashing algorithm, for example: md5, sha256, sha512
+# Use a secure hashing algorithm, for example, md5, sha256, sha512
 define('PRIVATE_KEY', md5('your-private-key-value'));
