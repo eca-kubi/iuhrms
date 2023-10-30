b142ab5088e6dbd0148ad9def14a81def9a96de2
eca-kubi <ecakubi@gmail.com>
2023-10-30 08:28:48 +0000

    Print the IP of the mysql container on startup

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -26,13 +26,30 @@ class ApiController extends Controller
         $roomTypeController->handleRequest($id);
     }
 
-    public function test(): void
+    public function test(string $dbhost): void
     {
-        $testController = new TestAPIController();
+        // Test the database connection
+
         try {
-            $testController->handleRequest(null);
+            // Create connection
+            $conn = new mysqli($dbhost, DB_USER, DB_PASSWORD, DB_NAME);// Check connection
+            if ($conn->connect_error) {
+                // Output connection parameters
+                echo "DB_HOST: " . $dbhost . PHP_EOL;
+                echo "DB_USER: " . DB_USER . PHP_EOL;
+                echo "DB_PASSWORD: " . DB_PASSWORD . PHP_EOL;
+                echo "DB_NAME: " . DB_NAME . PHP_EOL;
+                die("Connection failed: " . $conn->connect_error);
+            } else {
+                echo "Connected successfully" . PHP_EOL;
+            }
         } catch (Exception $e) {
-            echo $e->getMessage();
+            // Output connection parameters
+            echo "DB_HOST: " . $dbhost . PHP_EOL;
+            echo "DB_USER: " . DB_USER . PHP_EOL;
+            echo "DB_PASSWORD: " . DB_PASSWORD . PHP_EOL;
+            echo "DB_NAME: " . DB_NAME . PHP_EOL;
+            echo "Error: " . $e->getMessage();
         }
 
     }
