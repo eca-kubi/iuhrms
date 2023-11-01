9053251851b1b290c93eb3be6204e9354c2f9b6a
eca-kubi <ecakubi@gmail.com>
2023-11-01 14:10:38 +0000

    Use new certificates.

--- startup.sh
+++ startup.sh
@@ -7,15 +7,15 @@ if [[ -z "${SSL_CERTIFICATE}" ]] || [[ -z "${SSL_KEY}" ]]; then
 fi
 
 # Write the SSL certificate and key to files
-echo "${SSL_CERTIFICATE}" | perl -pe 's/@@/\n/g; s/##/\r/g' > /etc/ssl/certs/ssl-cert.pem
-cat /etc/ssl/certs/ssl-cert.pem
+echo "${SSL_CERTIFICATE}" | perl -pe 's/@@/\n/g; s/##/\r/\n/g' > /etc/ssl/certs/ssl-cert.crt
+cat /etc/ssl/certs/ssl-cert.crt
 
 # Replace the newline and  carriage return placeholders with the actual characters
 echo "${SSL_KEY}" | perl -pe 's/@@/\n/g; s/##/\r/g' > /etc/ssl/private/ssl-key.key
 cat /etc/ssl/private/ssl-key.key
 
 # Ensure the permissions are secure
-chmod 644 /etc/ssl/certs/ssl-cert.pem
+chmod 644 /etc/ssl/certs/ssl-cert.crt
 chmod 640 /etc/ssl/private/ssl-key.key
 
 # Update the Apache SSL configuration to use the certificate and key files
@@ -29,7 +29,7 @@ echo "
 <VirtualHost *:443>
     # Settings for HTTPS (port 443)
     SSLEngine on
-    SSLCertificateFile /etc/ssl/certs/ssl-cert.pem
+    SSLCertificateFile /etc/ssl/certs/ssl-cert.crt
     SSLCertificateKeyFile /etc/ssl/private/ssl-key.key
     ServerName ${APP_HOST}
     SSLProtocol TLSv1.2
